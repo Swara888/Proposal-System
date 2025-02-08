424d417c2b3fde4d9315d5fc4cd30afd4bf2c14b
Swara888 <swaralisuryawanshi123@gmail.com>
2025-02-08 17:55:43 +0530

    Update Proposal.sol

--- Proposal.sol
+++ Proposal.sol
@@ -1,4 +1,16 @@
-struct Proposal {
-    string title;
-    string description;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ProposalContract {
+    struct Proposal {
+        string title;
+        string description;
+    }
+
+    Proposal[] public proposals;
+
+    function create(string memory _title, string memory _description) public {
+        proposals.push(Proposal(_title, _description));
+    }
 }
+Explanation:
